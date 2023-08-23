f6348ca1cb0cf8433dd68620931a828020d9e954
Peter <petergreen880@yahoo.com>
2023-08-22 22:28:22 -0400

    Update BallSpawner.cs

--- Pong VR/Assets/Scripts/BallSpawner.cs
+++ Pong VR/Assets/Scripts/BallSpawner.cs
@@ -25,3 +25,35 @@ public class BallSpawner : MonoBehaviour
     }
 }
 
+/*
+New Ball Spawner
+
+using System.Collections;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    public float spawnDelay = 2.0f;
+    public float ballSpeed = 5.0f;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnBall());
+    }
+
+    private IEnumerator SpawnBall()
+    {
+        yield return new WaitForSeconds(spawnDelay);
+
+        GameObject ball = Instantiate(ballPrefab, transform.position, Quaternion.identity);
+        Rigidbody ballRigidbody = ball.GetComponent();
+
+        Vector3 randomDirection = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+        ballRigidbody.velocity = randomDirection * ballSpeed;
+    }
+}
+
+
+*/
+
