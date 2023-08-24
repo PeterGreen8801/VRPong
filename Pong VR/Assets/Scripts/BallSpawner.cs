7cf3a0c335a87c3a75b0c4ce52f8ddb7d553907d
Peter <petergreen880@yahoo.com>
2023-08-24 17:59:30 -0400

    Update BallSpawner.cs

--- Pong VR/Assets/Scripts/BallSpawner.cs
+++ Pong VR/Assets/Scripts/BallSpawner.cs
@@ -5,35 +5,6 @@ using UnityEngine;
 public class BallSpawner : MonoBehaviour
 {
     public GameObject ballPrefab;
-    public float initialForce = 5f;
-    public Vector2 spawnPosition;
-
-    private void Start()
-    {
-        SpawnBall();
-    }
-
-    //This probs wont work because its trying to use a vector2, can change to vector 3 and it will prob work
-    private void SpawnBall()
-    {
-        GameObject newBall = Instantiate(ballPrefab, spawnPosition, Quaternion.identity);
-        Rigidbody2D ballRigidbody = newBall.GetComponent<Rigidbody2D>(); //Had to add <Rigidbody2D>
-
-        float angle = Random.Range(30, 150) * Mathf.Deg2Rad;
-        Vector2 forceDirection = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
-        ballRigidbody.AddForce(forceDirection * initialForce, ForceMode2D.Impulse);
-    }
-}
-
-/*
-New Ball Spawner
-
-using System.Collections;
-using UnityEngine;
-
-public class BallSpawner : MonoBehaviour
-{
-    public GameObject ballPrefab;
     public float spawnDelay = 2.0f;
     public float ballSpeed = 5.0f;
 
@@ -54,6 +25,4 @@ public class BallSpawner : MonoBehaviour
     }
 }
 
-
-*/
 
