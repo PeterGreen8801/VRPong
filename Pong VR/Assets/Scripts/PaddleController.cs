53f02a847bdf8fa9ca9e03a66ff083bd692b2399
Peter <petergreen880@yahoo.com>
2023-08-26 18:03:17 -0400

    Edited size of colliders

--- Pong VR/Assets/Scripts/PaddleController.cs
+++ Pong VR/Assets/Scripts/PaddleController.cs
@@ -5,30 +5,7 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class PaddleController : MonoBehaviour
 {
-    private XRGrabInteractable grabInteractable;
-    private ConfigurableJoint joint;
-    private Rigidbody paddleRigidbody;
 
-    private void Awake()
-    {
-        grabInteractable = GetComponent<XRGrabInteractable>();
-        joint = GetComponent<ConfigurableJoint>();
-        paddleRigidbody = GetComponent<Rigidbody>();
-    }
-
-    private void Start()
-    {
-        joint.connectedBody = paddleRigidbody;
-    }
-
-    private void Update()
-    {
-        if (grabInteractable.isSelected)
-        {
-            Vector3 newPosition = new Vector3(transform.position.x, grabInteractable.selectingInteractor.transform.position.y, transform.position.z);
-            joint.targetPosition = newPosition;
-        }
-    }
 }
 
 
